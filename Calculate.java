fcb14cbc0d29f627171f011be3bdacf08075d956
Loshok <alysha1208@yandex.ru>
2016-07-20 00:30:03 +0300

    Calculate22222mathpow

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,11 @@
 public class Calculate{
-	public static void main(String args[]){
-		System.out.print("«дорово мудень!");
+public static void main(String args[]){
+System.out.println("«дорово мудень! ¬веди числа");
+double first = Double.valueOf(args[0]);
+double second = Integer.valueOf(args[1]);
+double summ = first + second/2 + second*5 + Math.pow(6,3);
+System.out.println("Summ is " + summ);
+		
 }
 
 
